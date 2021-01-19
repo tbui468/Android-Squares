589368ee07527442d53fe36054b439a8a5a279cd
Thomas Bui <thomasthienbui@gmail.com>
2021-01-19 12:49:54 +0700

    fixed bug with app crashing when pushing back button on login screen.  Added basic facebook login

--- app/src/main/java/com/example/androidsquares/MainActivity.kt
+++ app/src/main/java/com/example/androidsquares/MainActivity.kt
@@ -1,18 +1,31 @@
 //get a complete vertical slice with two puzzle cubes
 
-    //facebook login page (or choose to play without online data)
-        //this is the default view on opening the app
-        //transition to the game view after logging in/choosing not to log in
-
-    //cache test data on server - name, profile pic, and (set, puzzle, transformation count in solution)
-        //try testing by logging in with my account
-
-    //read test friend data from server and display profile pics of friends
+    //Bug: when pushing back button while at login screen, the app crashes
 
     //complete puzzle set 5: similar to set 3, make a few variations of puzzles - can mix them up later
-        //3 adjacent 2x2 squares variation
-        //3 adjacent above and two adjacent below (or 1)
-        //make puzzles where user has to connect 3 dark blocks (don't have too many of those yet) (including 2+ colors)
+    //3 adjacent 2x2 squares variation
+    //3 adjacent above and two adjacent below (or 1)
+    //make puzzles where user has to connect 3 dark blocks (don't have too many of those yet) (including 2+ colors)
+
+    //seems to be a problem with setting permission and loggging in (it's doing it twice)
+        //push facebook login button passes the baton to CallbackManager which has user login with name/avatar permission
+        // then comes back and executes my code in setButtonListener (which adds email permission)
+        //this causes facebook to go back to login page with name/avatar/email permission
+            //might need to see the other option for loggin in on the documentation and not rely on CallbackManager
+
+    //build a quick and dirty implementation of the steps below:
+        //get fb_user_id and access_token on android (both strings)
+            //I have the access_token - how to get fb_user_name?
+        //send http request to webserver to authenticate user (by issuing Graph API request using fb_user_name and access_token sent to from android device)
+            //let's use Nodejs, express and TypeScript to set up web server - no need for html/css since we only need to send http requests with two parameters
+            //web server has two forms - fb_user_id, and access_token.  use curl to send http request to Graph API with this
+        //if fb_user_id and access_token verified, 'log in' to the server database using fb_user_name (and now can alter database entries for this user)
+        //when user solves a puzzle/saves data, write data (such as transformations taken to solve puzzles) to database
+
+    //make transition from login page to game page more natural (transition buttons off-screen and then transitioning sets on-screen, for example)
+
+    //reorder puzzles into more interesting format (instead of the rows and columns they are in now)
+        //this will also allow user to choose the next puzzle, giving them more agency and choice
 
     //Back button is normal to have in the app - keep it there since it's better to have an in-app way to go back (but include android back button functionality too)
 
@@ -38,7 +51,6 @@
 
     //tutorial for swap, pinch in, pinch out, rotate, reflect
         //have a dot animate showing finger motion to perform certain action
-
 
     //puzzle goal should be obvious when looking at a puzzle for the first time
         //user should know exactly where the colored squares need to go to connect all the colors
@@ -76,39 +88,86 @@
 
 package com.example.androidsquares
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.util.Log
 import android.os.Bundle
+import com.facebook.AccessToken
+
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 
 class MainActivity: AppCompatActivity() {
     private lateinit var mSquaresSurfaceView: SquaresSurfaceView
+    private var mOnLogin = true
     private lateinit var mSkipButton: Button
     private lateinit var mLoginButton: Button
+    private lateinit var mCallbackManager: CallbackManager
 
     public override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("hmm", "onCreate called")
         setTheme(R.style.SplashScreen)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+        initLoginView()
+    }
+
+    private fun initLoginView() {
+        mSquaresSurfaceView = SquaresSurfaceView(this)
 
         mSkipButton = findViewById(R.id.skip_button)
         mLoginButton = findViewById(R.id.login_button)
 
         //login to facebook
         mLoginButton.setOnClickListener {
+            LoginManager.getInstance().logInWithReadPermissions(this, mutableListOf("email"))
         }
+
+        mCallbackManager = CallbackManager.Factory.create()
+
+        //note: object keyword here allows creation of new subclass that can then be overriden (?)
+        LoginManager.getInstance().registerCallback(mCallbackManager, object: FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d("facebooktest", "success")
+                val accessToken: AccessToken? = AccessToken.getCurrentAccessToken()
+                val isLoggedIn = accessToken != null && !accessToken.isExpired
+                Log.d("facebooktest", isLoggedIn.toString())
+            }
+            override fun onCancel() {
+                Log.d("facebooktest", "cancel")
+            }
+            override fun onError(error: FacebookException) {
+                Log.e("facebooktest", "error")
+            }
+        })
 
         //skip loggin into facebook
         mSkipButton.setOnClickListener {
-            mSquaresSurfaceView = SquaresSurfaceView(this)
             setContentView(mSquaresSurfaceView)
+            mOnLogin = false
         }
     }
 
+
     override fun onBackPressed() {
-        if(mSquaresSurfaceView.renderer.getScreenState() == Screen.Set)
-            super.onBackPressed() //exit app
-        else
+        if(!mOnLogin && mSquaresSurfaceView.renderer.getScreenState() == Screen.Set) {
+            setContentView(R.layout.login)
+            initLoginView()
+            mOnLogin = true
+        }else if(mOnLogin) {
+            super.onBackPressed()
+        }else {
             mSquaresSurfaceView.renderer.mInputQueue.add(InputData(TouchType.Back, 0f, 0f, 0.3f))
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     public override fun onResume() {
